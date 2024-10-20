6dcc0e2f1033f6fcafc37f1c01f6b0daea8964fb
DANI-PC\dani- <dani-15-94@hotmail.com>
2024-10-20 07:24:54 +0200

    Auth added in client

---
+++ BlazorWebAssemblyTemplate.Client/Auth/AuthService.cs
@@ -0,0 +1,26 @@
+﻿using BlazorWebAssemblyTemplate.Shared.Auth;
+using System.Net.Http.Json;
+
+namespace BlazorWebAssemblyTemplate.Client.Auth
+{
+    public interface IAuthService
+    {
+        Task<AuthenticationStatus> GetAuthenticationStatus();
+    }
+
+    public class AuthService(HttpClient httpClient) : IAuthService
+    {
+        private readonly HttpClient _httpClient = httpClient;
+
+        public async Task<AuthenticationStatus> GetAuthenticationStatus()
+        {
+            var response = await _httpClient.GetAsync("api/auth/status");
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<AuthenticationStatus>();
+            }
+
+            return new AuthenticationStatus { IsAuthenticated = false };
+        }
+    }
+}
--- BlazorWebAssemblyTemplate.Client/Program.cs
+++ BlazorWebAssemblyTemplate.Client/Program.cs
@@ -4,12 +4,14 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazorise;
 using Blazorise.AntDesign;
 using Blazorise.Icons.FontAwesome;
+using BlazorWebAssemblyTemplate.Client.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services
     .AddBlazorise(options =>
